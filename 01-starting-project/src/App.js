e77acc10cf14efbc79bf79b1acac79a135cb9e52
fospwd <fospwd@umsystem.edu>
2023-10-19 12:05:36 -0500

    first

---
+++ 01-starting-project/src/App.js
@@ -0,0 +1,59 @@
+import Header from './components/Header/Heading';
+import UserInput from './components/UserInput.js/Userinput';
+import UserTable from './components/ResultsTable/Table';
+import { useState } from 'react';
+function App() {
+  const [userInput, setUserInput] =  useState(null)
+  // const [results, setResults] =  useState(null)
+
+
+  const calculateHandler = (userInput) => {
+   setUserInput(userInput);
+    // Should be triggered when form is submitted
+    // You might not directly want to bind it to the submit event on the form though...
+
+    // setResults(yearlyData);
+
+
+  };
+
+  const yearlyData = []; // per-year results
+
+  if(userInput){
+    let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
+    const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
+    const expectedReturn = +userInput['expected-return'] / 100;
+    const duration = +userInput['duration'];
+  
+
+    // The below code calculates yearly results (total savings, interest etc)
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        // feel free to change the shape of the data pushed to the array!
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
+  }
+
+
+  return (
+    <div>
+    <Header/>
+       <UserInput onCalculate={calculateHandler}/>
+       {!userInput && <p style={{textAlign: 'center'}}> no investment calculated yet</p>}
+       {userInput && <UserTable  data={yearlyData}  initialInvestment = {userInput['current-savings']}/>}
+
+      {/* Todo: Show below table conditionally (only once result data is available) */}
+      {/* Show fallback text if no data is available */}
+
+
+    </div>
+  );
+}
+
+export default App;
